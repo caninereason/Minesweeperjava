fd8c91a0a66edef1b767ddbc70d9cb3a2873cf7d
caninereason <theciaranneeson@hotmail.com>
2022-10-05 00:31:12 +0100

    initial js bombs

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,14 @@
 document.addEventListener('DOMContentLoaded',() =>{
     const grid = document.querySelector('.grid')
     let width =10
+    let bombAmount=20
     let squares = []
     //create Board
-    function createBoard()
-{
+    function createBoard(){
+        //get shuffled game array
+        const bombsArray = Array(bombAmount).fill('bomb')
+        const emptyArray = Array(width*width - bombAmount).fill('valid')
+        console.log(bombsArray)
     for(let i =0;i<width*width;i++){
         const square =document.createElement('div')
         square.setAttribute('id',i)
