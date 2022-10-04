1f1aafa7b631b0423b657940a5d700a0b23c487d
caninereason <theciaranneeson@hotmail.com>
2022-10-05 00:39:07 +0100

    bombs randomised

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,12 +9,12 @@ document.addEventListener('DOMContentLoaded',() =>{
         const bombsArray = Array(bombAmount).fill('bomb')
         const emptyArray = Array(width*width - bombAmount).fill('valid')
         const gameArray = emptyArray.concat(bombsArray)
-        console.log(gameArray)
         const shuffledArray = gameArray.sort(() => Math.random() - 0.5)
-        console.log(shuffledArray)
+       
     for(let i =0;i<width*width;i++){
         const square =document.createElement('div')
-        square.setAttribute('id',i)
+        square.setAttribute('id', i)
+        square.classList.add(shuffledArray[i])
         grid.appendChild(square)
         squares.push(square)
 }
