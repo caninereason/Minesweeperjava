73b05063399d21dee2efb3b210651590687283df
caninereason <theciaranneeson@hotmail.com>
2022-10-14 20:06:29 +0100

    added difficulty levels

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     let grid = document.querySelector('.grid')
-    let scale =document.getElementById("form").elements["num"].valueAsNumber
+    // let scale =document.getElementById("form").elements["num"].valueAsNumber
+    let scale=0 
     let flagsLeft = document.querySelector('#flags-left')
     let result =document.querySelector('#result')
     let bombs =5  +(scale*5)
@@ -12,6 +13,73 @@ document.addEventListener('DOMContentLoaded', () => {
     grid.style.width = width*"40"+"px"
     grid.style.height = width*"40"+"px"
     flagsLeft.innerHTML = bombs - flags
+    var modal = document.getElementById("myModal");
+
+    // Get the button that opens the modal
+    var btn = document.getElementById("myBtn");
+    var Easy = document.getElementById("Easy");
+    var Medium = document.getElementById("Medium");
+    var Hard = document.getElementById("Hard");
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+    
+    // When the user clicks on the button, open the modal
+    btn.onclick = function() {
+      modal.style.display = "block";
+    }
+    Easy.onclick = function() {
+        isGameOver = false
+        scale =0;
+        width = (10 +scale)
+        bombs =5  +(scale*5)
+        flagsLeft.innerHTML = bombs - flags
+        grid.style.width = width*"40"+"px"
+        grid.style.height = width*"40"+"px"
+        grid.innerHTML=''
+        sqs=[]
+        createBoard()
+        modal.style.display = "none";
+        console.log(scale)
+      }
+      Medium.onclick = function() {
+        isGameOver = false
+        scale =2;
+        width = (10 +scale)
+         bombs =5  +(scale*5)
+         flagsLeft.innerHTML = bombs - flags
+        grid.style.width = width*"40"+"px"
+        grid.style.height = width*"40"+"px"
+        grid.innerHTML=''
+        sqs=[]
+        createBoard()
+        modal.style.display = "none";
+        console.log(scale)
+      }
+      Hard.onclick = function() {
+        isGameOver = false
+        scale =5;
+        width = (10 +scale)
+         bombs =5  +(scale*5)
+         flagsLeft.innerHTML = bombs - flags
+        grid.style.width = width*"40"+"px"
+        grid.style.height = width*"40"+"px"
+        grid.innerHTML=''
+        sqs=[]
+        createBoard()
+        modal.style.display = "none";
+        console.log(scale)
+      }
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+      modal.style.display = "none";
+    }
+    
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    } 
     //create Board
     function createBoard() {
      //   console.log()
@@ -63,7 +131,7 @@ document.addEventListener('DOMContentLoaded', () => {
         }
 
     }
-    createBoard()
+    //createBoard()
 
     function addFlag(sq) {
         if (isGameOver) return
@@ -212,30 +280,7 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
     
-    var modal = document.getElementById("myModal");
-
-    // Get the button that opens the modal
-    var btn = document.getElementById("myBtn");
-    
-    // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
-    
-    // When the user clicks on the button, open the modal
-    btn.onclick = function() {
-      modal.style.display = "block";
-    }
-    
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
-      modal.style.display = "none";
-    }
-    
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
-      if (event.target == modal) {
-        modal.style.display = "none";
-      }
-    } 
+  
 
 
 
